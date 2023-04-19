7d6c9c142dac232534396a2e26d3223992c6d13a
Vladimir <zonda4242@gmail.com>
2023-04-19 23:19:51 +0200

    Initial Commit

---
+++ hw1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw1
+//
+//  Created by Владимир Клевцов on 19.4.23..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
